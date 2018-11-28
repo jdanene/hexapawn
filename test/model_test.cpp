2361ccf2a8f08d00f4c46f1249771c68ad549603
“jdanene” <dominicanene@gmail.com>
2018-11-27 20:25:54 -0600

    Latest tests

--- test/model_test.cpp
+++ test/model_test.cpp
@@ -1,110 +1,270 @@
 #include <model.h>
-//#include <controller.h>
+#include <controller.h>
 #include <view.h>
 #include <catch.h>
 #include <iostream>
 
+
 using namespace hexapawn;
-TEST_CASE("Check that defaults are correct") {
-    Model hexapawn(8, 8);
-    
-    //Ensure that the bottom row corresponds to P1 and top row to P2
-    CHECK(hexapawn.get_ele({0, 0}) == Player::second);
+
+TEST_CASE("Make a chessboard")
+{
+    Model hexapawn(4, 4);
+    CHECK(hexapawn.width() == 4);
+    CHECK(hexapawn.height() == 4 );
+    CHECK(hexapawn.get_turn() == Player::first);
+}
+
+TEST_CASE("Is a viable move?")
+{
+    Model hexapawn(3,3);
+    CHECK(hexapawn.is_viable_p({2,0}, {1,0}, Player::first) == false);
+    CHECK(hexapawn.is_viable_p({2,1}, {1,0}, Player::first) == false);
+    CHECK(hexapawn.is_viable_p({2,2}, {2,1}, Player::first) == false);
+    CHECK(hexapawn.is_viable_p({0,0}, {0,1}, Player::second) == false);
+    CHECK(hexapawn.is_viable_p({2,2}, {2,1}, Player::second) == true);
+    CHECK(hexapawn.is_viable_p({1,1}, {1,2}, Player::second) == false);
+}
+
+TEST_CASE("Is a pawn allowed?")
+{
+    Model hexapawn(3,3);
+    CHECK(hexapawn.pawn_there_p({1,2}) == true);
+    CHECK(hexapawn.pawn_there_p({2,2}) == true);
+    CHECK_THROWS_AS(hexapawn.pawn_there_p({3,0}), ge211::Client_logic_error);
+}
+
+TEST_CASE("Is there a pawn in chessboard?")
+{
+    Model hexapawn(4,4);
+    CHECK(hexapawn.pawn_there_p({1,2}) == false);
+    CHECK(hexapawn.pawn_there_p({1,0}) == true);
+    CHECK(hexapawn.pawn_there_p({3,3}) == true);
+    CHECK_THROWS_AS(hexapawn.pawn_there_p({4,0}), ge211::Client_logic_error);
 }
 
 
-
-
-// Correct turn?
-// CHECK( hexapawn.get_turn() == Player::second);
-// Correct winner?
-//CHECK( hexapawn.game_over() == Player::neither);
-
-
-
-//Viable Moves
-//CHECK(connect_four.is_viable_p({0,7},{0,6},Player::first)); //P1: forward to an empty square
-//CHECK(connect_four.is_viable_p({0,0},{0,1},Player::second)); //P2: forward to an empty square
-
-//Not Viable Moves
-//CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P1: left boundary pawn {0,7} => to upper left kill {-1,6}
-//CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P1: right boundary pawn {4,7} => to upper right kill {5,6}
-
-
-
-
-//CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P1: pawn => upper left kill without enemy pawn
-//CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P1: pawn => upper right kill without enemy pawn
-//CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P2: pawn => upper left kill without enemy pawn
-// CHECK(!connect_four.is_viable_p({0,7},{0,6},Player::first)); //P2: pawn => upper right kill without enemy pawn
-
-
-
-
-/*
-TEST_CASE("can create other sizes")
+//Ensure that the bottom row corresponds to P1 and top row to P2
+TEST_CASE("Check pawns in chessboard")
 {
-Model c2(2, 5, 4);
-CHECK( c2.k() == 2 );
-CHECK( c2.m() == 5 );
-CHECK( c2.n() == 4 );
+    Model hexapawn(8,8);
+    CHECK(hexapawn.get_ele({0,0}) == Player::first);
 
-Model c3(3, 6, 5);
-CHECK( c3.k() == 3 );
-CHECK( c3.m() == 6 );
-CHECK( c3.n() == 5 );
-
-Model c5(5, 8, 7);
-CHECK( c5.k() == 5 );
-CHECK( c5.m() == 8 );
-CHECK( c5.n() == 7 );
 }
 
-TEST_CASE("can move")
+
+TEST_CASE("Game over: draw")
 {
-using Column = std::vector<Player>;
+    Model hexapawn(3,3);
 
-Model c2(2, 5, 4);
+    std::cout << "Test: Game over due to draw." << "\n";
 
-CHECK( c2.get_turn() == Player::first );
-CHECK( c2.get_winner() == Player::neither );
-c2.place_token(0);
-CHECK( c2.get_column(0) == Column{Player::first} );
+    /* Below is the logical board that we will test: PLAYER1
+     *         0   1   2
+     *
+     *     0  | W| - | W |
+     *     1  | -| W | - |
+     *     2  | B| B | B |
+     *   PLAYER1: {1,0} => {1,1}
+     * */
+    /// P1: {1,0} => {1,1}
+    std::cout << "\n";
+    std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+    std::cout << "Before move "<< "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    std::cout << "Element {1,2} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Viable? {1,2} =>  {1,1} is: "<< hexapawn.is_viable_p({1,0},{1,1},hexapawn.get_turn())<< "\n";
+    hexapawn.place_pawn({1,0},{1,1});
+    std::cout << "After move "<< "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    std::cout << "Element {1,2} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
 
-CHECK( c2.get_turn() == Player::second );
-c2.place_token(1);
-CHECK( c2.get_column(1) == Column{Player::second} );
+    /* Below is the logical board that we will test
+     *         0   1   2
+     *
+     *     0  | W| - | W |
+     *     1  | -| B | - |
+     *     2  | B| B | - |
+     * PLAYER2: {2,2} => {1,1}
+     */
+     /// [Update Turn: P1 => P2] AND [Check if winner: No winner => No winner]
+     hexapawn.update_winner_and_turn();
+     std::cout << "\n";
+     std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+     std::cout << "Before move "<< "\n";
+     std::cout << "Element {0,0} is: "<< as_integer(hexapawn.get_ele({2,2})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     hexapawn.place_pawn({2,2},{1,1});
+     std::cout << "After move "<< "\n";
+     std::cout << "Element {0,0} is: "<< as_integer(hexapawn.get_ele({2,2})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
 
-CHECK( c2.get_turn() == Player::first );
-c2.place_token(2);
-CHECK( c2.get_column(2) == Column{Player::first} );
+//    /* Below is the logical board that we will test: PLAYER1
+//     *         0   1   2
+//     *
+//     *     0  | -| - | W |
+//     *     1  | -| W | - |
+//     *     2  | B| B | - |
+//     *  Player1: {0,0} => {1,1}
+//     *****/
+//
+     hexapawn.update_winner_and_turn();
+     std::cout << "\n";
+     std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+     std::cout << "Before move "<< "\n";
+     std::cout << "Element {2,2} is: "<< as_integer(hexapawn.get_ele({0,0})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     hexapawn.place_pawn({0,0},{1,1});
+     std::cout << "After move "<< "\n";
+     std::cout << "Element {2,2} is: "<< as_integer(hexapawn.get_ele({0,0})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
 
-CHECK( c2.get_turn() == Player::second );
-c2.place_token(0);
-CHECK( c2.get_column(0) == Column{Player::first, Player::second} );
+//    /* Below is the logical board that we will test:
+//     *         0   1   2
+//     *
+//     *     0  | -| - | W |
+//     *     1  | -| B | - |
+//     *     2  | -| B | - |
+//     *  - Player 2: {0,2}=> {1,1}
+//     *****/
+//
+     hexapawn.update_winner_and_turn();
+     std::cout << "\n";
+     std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+     std::cout << "Before move "<< "\n";
+     std::cout << "Element {0,2} is: "<< as_integer(hexapawn.get_ele({0,2})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     hexapawn.place_pawn({0,2},{1,1});
+     std::cout << "After move "<< "\n";
+     std::cout << "Element {2,0} is: "<< as_integer(hexapawn.get_ele({0,2})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
+     std::cout << "Game winner? : "<< as_integer(hexapawn.game_winner()) << "\n";
 
-CHECK( c2.get_turn() == Player::neither );
-CHECK( c2.get_winner() == Player::second );
-
-CHECK_THROWS_AS(c2.place_token(0), ge211::Client_logic_error);
+//    /* Below is the logical board that we will test:
+//     *         0   1   2
+//     *
+//     *     0  | -| - | - |
+//     *     1  | -| w | - |
+//     *     2  | -| B | - |
+//     *  - Player 1: {2,0}=> {1,1}
+//     *  - Draw
+//     *****/
+//
+     hexapawn.update_winner_and_turn();
+     std::cout << "\n";
+     std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+     std::cout << "Before move "<< "\n";
+     std::cout << "Element {2,0} is: "<< as_integer(hexapawn.get_ele({2,0})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+     hexapawn.place_pawn({2,0},{1,1});
+     std::cout << "After move "<< "\n";
+     std::cout << "Element {2,0} is: "<< as_integer(hexapawn.get_ele({2,0})) << "\n";
+     std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    //End in a draw good!
+     hexapawn.update_winner_and_turn();
+     std::cout << "New Player turn is: "<< as_integer(hexapawn.get_turn()) << "\n";
+     std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
+     std::cout << "Game winner? : "<< as_integer(hexapawn.game_winner()) << "\n";
+     std::cout << "\n";
+     std::cout << "\n";
 }
 
-TEST_CASE("full column throws")
+
+TEST_CASE("Game over: winner")
 {
-Model c2(2, 5, 4);
+    Model hexapawn(3,3);
 
-c2.place_token(0);
-c2.place_token(0);
-c2.place_token(0);
-c2.place_token(0);
-CHECK_THROWS_AS(c2.place_token(0), ge211::Client_logic_error);
+    std::cout << "Test: Game over due to winner." << "\n";
 
-c2.place_token(2);
-c2.place_token(2);
-c2.place_token(2);
-c2.place_token(2);
-CHECK_THROWS_AS(c2.place_token(2), ge211::Client_logic_error);
+    /* Below is the logical board that we will test: PLAYER1
+     *         0   1   2
+     *
+     *     0  | w| - | w |
+     *     1  | -| w | - |
+     *     2  | B| B | B |
+     *   PLAYER1: {1,0} => {1,1}
+     * */
+    /// P1: {1,0} => {1,1}
+    std::cout << "\n";
+    std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+    std::cout << "Before move "<< "\n";
+    std::cout << "Element {1,0} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Viable? {1,2} =>  {1,1} is: "<< hexapawn.is_viable_p({1,2},{1,1},hexapawn.get_turn())<< "\n";
+    hexapawn.place_pawn({1,0},{1,1});
+    std::cout << "After move "<< "\n";
+    std::cout << "Element {1,0} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
 
-c2.place_token(1);
-CHECK_THROWS_AS(c2.place_token(1), ge211::Client_logic_error);
-}*/
+    /* Below is the logical board that we will test
+     *         0   1   2
+     *
+     *     0  | W| - | W |
+     *     1  | -| B | - |
+     *     2  | -| B | B |
+     * PLAYER2: {0,2} => {1,1}
+     */
+    /// [Update Turn: P1 => P2] AND [Check if winner: No winner => No winner]
+    /// P2: {0,2} => {1,1}
+    hexapawn.update_winner_and_turn();
+    std::cout << "\n";
+    std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+    std::cout << "Before move "<< "\n";
+    std::cout << "Element {0,2} is: "<< as_integer(hexapawn.get_ele({0,2})) << "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    hexapawn.place_pawn({0,2},{1,1});
+    std::cout << "After move "<< "\n";
+    std::cout << "Element {0,0} is: "<< as_integer(hexapawn.get_ele({0,2})) << "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
+
+//    /* Below is the logical board that we will test: PLAYER1
+//     *         0   1   2
+//     *
+//     *     0  | -| - | W |
+//     *     1  | W| B | - |
+//     *     2  | -| B | B |
+//     *  Player1: {0,0} => {0,1}
+//     *****/
+//
+    hexapawn.update_winner_and_turn();
+    std::cout << "\n";
+    std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+    std::cout << "Before move "<< "\n";
+    std::cout << "Element {0,0} is: "<< as_integer(hexapawn.get_ele({0,0})) << "\n";
+    std::cout << "Element {0,1} is: "<< as_integer(hexapawn.get_ele({0,1})) << "\n";
+    hexapawn.place_pawn({0,0},{0,1});
+    std::cout << "After move "<< "\n";
+    std::cout << "Element {0,0} is: "<< as_integer(hexapawn.get_ele({0,0})) << "\n";
+    std::cout << "Element {0,1} is: "<< as_integer(hexapawn.get_ele({0,1})) << "\n";
+    std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
+
+//    /* Below is the logical board that we will test:
+//     *         0   1   2
+//     *
+//     *     0  | -| B | W |
+//     *     1  | W| - | - |
+//     *     2  | -| B | B |
+//     *  - Player 2: {1,1}=> {1,0}
+//     *****/
+//
+    hexapawn.update_winner_and_turn();
+    std::cout << "\n";
+    std::cout << "Who's turn: "<< as_integer(hexapawn.get_turn()) << "\n";
+    std::cout << "Before move "<< "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Element {1,0} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    hexapawn.place_pawn({1,1},{1,0});
+    hexapawn.update_winner_and_turn();
+    std::cout << "After move "<< "\n";
+    std::cout << "Element {1,1} is: "<< as_integer(hexapawn.get_ele({1,1})) << "\n";
+    std::cout << "Element {1,0} is: "<< as_integer(hexapawn.get_ele({1,0})) << "\n";
+    std::cout << "Game over? : "<< hexapawn.game_over_p() << "\n";
+    std::cout << "Game winner? : "<< as_integer(hexapawn.game_winner()) << "\n";
+
+}
